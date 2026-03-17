bb34659ecf0ae723e10b27db34ce20c739387bb9
Dzarek <jarekjanas95@gmail.com>
2026-03-17 22:35:51 +0100

    feat: initial commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen">
+      <aside className="w-64 border-r p-4">Sidebar</aside>
+
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+}
